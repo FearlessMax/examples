042fb7117689523b9828ab4bd759a9f255361911
Maxim <taran12age@mail.ru>
2018-02-23 14:04:30 +0300

    task 1

--- kernel_modules/task_1/hello.c
+++ kernel_modules/task_1/hello.c
@@ -1,15 +1,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-MODULE_AUTHOR ("Druzhinin Maksim");
-MODULE_VERSION ("1.0");
 MODULE_LICENSE ("GPL");
+MODULE_AUTHOR ("Druzhinin Maxim");
+MODULE_VERSION ("1.0");
 
-int init_module (void) {
+int init_module (void)		// загрузка модуля
+{
 	printk (KERN_INFO "Hello, world!\n");
 	return 0;
 }
 
-void cleanup_module (void) {
+void cleanup_module (void)		// выгрузка модуля
+{
 	printk (KERN_INFO "Bye!\n");
 }
